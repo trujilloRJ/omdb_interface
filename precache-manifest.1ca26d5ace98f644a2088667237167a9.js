129b83d938567fba506a37e75636046d4279628f
unknown <trujillorodriguezjavier@gmail.com>
2020-02-12 17:33:00 +0100

    Updates

---
+++ precache-manifest.1ca26d5ace98f644a2088667237167a9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84db7eeafa5fec39ba07623008db7892",
+    "url": "/omdb_interface/index.html"
+  },
+  {
+    "revision": "7e2a350674b6b39a9da3",
+    "url": "/omdb_interface/static/css/main.6c9d78d5.chunk.css"
+  },
+  {
+    "revision": "911cd1252c4a1ece6fa1",
+    "url": "/omdb_interface/static/js/2.e1641b91.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/omdb_interface/static/js/2.e1641b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e2a350674b6b39a9da3",
+    "url": "/omdb_interface/static/js/main.9320be5b.chunk.js"
+  },
+  {
+    "revision": "5d7deb766acbba2ed2e0",
+    "url": "/omdb_interface/static/js/runtime-main.79200c7a.js"
+  },
+  {
+    "revision": "3d4027711725ec59b3ba41dcf572b72d",
+    "url": "/omdb_interface/static/media/imdb.3d402771.png"
+  },
+  {
+    "revision": "c9c11cc56dd71bb4c0423870cc3b5d3c",
+    "url": "/omdb_interface/static/media/metacritic.c9c11cc5.png"
+  },
+  {
+    "revision": "325ad745794a14f4bc6f7dc367cfb5a0",
+    "url": "/omdb_interface/static/media/rotten_tomatoes.325ad745.jpg"
+  }
+]);
